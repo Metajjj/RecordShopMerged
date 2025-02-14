cd9508919b003de2c731ac27b702b559df349a00
Metajjj <joolsjannaway@gmail.com>
2025-02-14 16:26:55 +0000

    learning media queries for CSS

--- RecordShopBlazor/FormValidationAttributes/ValidAttributes.cs
+++
@@ -1,9 +0,0 @@
-﻿
-using System.ComponentModel.DataAnnotations;
-
-public class ValidAttributes : ValidationAttribute
-{
-    [DataType(DataType.Date)]
-    public static DateOnly Date { get; set; } = new DateOnly();
-}
-
